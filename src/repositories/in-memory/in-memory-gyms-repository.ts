6dc3f41e0f731812829a6e748db3227cff67c68a
Lipe biondes <alefefelipebiondesmagalhaes@gmail.com>
2025-01-27 17:55:03 -0300

    feat: add repositories `gyms-repository` and `in-memory-gyms`

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,6 +1,7 @@
-import {Gym} from "@prisma/client";
+import {Gym, Prisma} from "@prisma/client";
 
 import {GymsRepository} from "@/repositories/gyms-repository";
+import {randomUUID} from "node:crypto";
 
 export class InMemoryGymsRepository implements GymsRepository {
     public items: Gym[] = [];
@@ -12,6 +13,21 @@ export class InMemoryGymsRepository implements GymsRepository {
         if (!gym) {
             return null;
         }
+        return gym;
+    }
+
+    async create(data: Prisma.GymCreateInput): Promise<Gym> {
+        const gym = {
+            id: data.id ?? randomUUID(),
+            title: data.title,
+            description: data.description ?? null,
+            phone: data.phone ?? null,
+            latitude: new Prisma.Decimal(data.latitude.toString()),
+            longitude: new Prisma.Decimal(data.longitude.toString()),
+        }
+
+        this.items.push(gym);
+
         return gym;
     }
 
