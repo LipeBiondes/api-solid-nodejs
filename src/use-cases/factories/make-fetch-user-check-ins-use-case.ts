275cd0bd34670f78179217253380007402ce9ecd
Lipe biondes <alefefelipebiondesmagalhaes@gmail.com>
2025-01-28 21:26:05 -0300

    feat: create factories to all use cases

---
+++ src/use-cases/factories/make-fetch-user-check-ins-use-case.ts
@@ -0,0 +1,7 @@
+import {FetchUserCheckInsHistoryUseCase} from "@/use-cases/fetch-user-check-ins-history";
+import {PrismaCheckInsRepository} from "@/repositories/prisma/prisma-check-ins-repository";
+
+export function makeFetchUserCheckInsUseCase() {
+    const checkInsRepository = new PrismaCheckInsRepository();
+    return new FetchUserCheckInsHistoryUseCase(checkInsRepository);
+}
